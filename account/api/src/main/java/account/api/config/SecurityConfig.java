4f42bde2647e78918866b30280cf3bc0615b0934
IsKJH <dkdlrh147@gmail.com>
2025-10-11 23:25:50 +0900

    fix: [김지한] CORS 설정 수정 및 Redis 컨테이너 추가

--- account/api/src/main/java/account/api/config/SecurityConfig.java
+++ account/api/src/main/java/account/api/config/SecurityConfig.java
@@ -48,10 +48,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+        configuration.setAllowedOriginPatterns(Arrays.asList(allowedOrigins.split(",")));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
