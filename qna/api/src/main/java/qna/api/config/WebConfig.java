50a8f7e223ca17112b81580c46184d842d243675
JUYOUNGLEE <duddud931215@gmail.com>
2025-10-17 20:44:46 +0900

    feat:[이주영]qna-backend controller add
    like, view count increase controller add

--- qna/api/src/main/java/qna/api/config/WebConfig.java
+++ qna/api/src/main/java/qna/api/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Value("${app.cors.allowed-origins}")
@@ -29,5 +28,4 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowCredentials(allowCredentials)
                 .maxAge(3600);
     }
-
 }
